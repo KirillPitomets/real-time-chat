3ac0116c9bb2e450323b677693df5dd45e034f7b
Kirill <apelsinka220405@gmail.com>
2025-12-16 04:38:00 +0200

    Fix: add .env.PUBLIC_API_URL

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,5 +1,6 @@
 import { treaty } from '@elysiajs/eden'
 import type { App } from '../app/api/[[...slugs]]/route'
 
+const API_URL = process.env.PUBLIC_API_URL
 // this require .api to enter /api prefix
-export const client = treaty<App>('real-time-chat-three-sepia.vercel.app').api
+export const client = treaty<App>(API_URL!).api
